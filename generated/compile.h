b96c1e72a513d6680c339a1bc8603eea8f28d852
root <root@sameer-Vostro-1015.(none)>
2014-04-18 21:03:57 -0400

    first commmit

---
+++ generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "i386"
+#define UTS_VERSION "#1 PREEMPT Fri Apr 11 23:54:16 EDT 2014"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "sameer-Vostro-1015"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
